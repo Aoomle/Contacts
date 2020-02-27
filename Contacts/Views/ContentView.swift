26d9b9577dd6a67ba8ed3ca15a4b710eec127976
Abdulmalik <abdulmmbs@gmail.com>
2020-02-27 05:57:59 +0100

    Add searchbar

--- Contacts/Views/ContentView.swift
+++ Contacts/Views/ContentView.swift
@@ -9,26 +9,68 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var searchText = ""
+  @State private var showCancelButton: Bool = false
+  
   var contactStore: ContactStore
     var body: some View {
-      NavigationView {
-        List(contactStore.contacts) { contact in
-          Text(contact.name)
-        }
-        .navigationBarTitle("Contact")
-        .navigationBarItems(
-          leading:
-            Button("Groups"){
-        },
-          trailing:
-          Button(
-            action: {}
-          ){
-            Image(systemName: "plus")
+  
+        NavigationView {
+          
+          VStack {
+          // Search view
+            HStack {
+              HStack {
+                  Image(systemName: "magnifyingglass")
+
+                  TextField("search", text: $searchText, onEditingChanged: { isEditing in
+                      self.showCancelButton = true
+                  }, onCommit: {
+                      print("onCommit")
+                  }).foregroundColor(.primary)
+
+                  Button(action: {
+                      self.searchText = ""
+                  }) {
+                      Image(systemName: "xmark.circle.fill").opacity(searchText == "" ? 0 : 1)
+                  }
+              }
+              .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+              .foregroundColor(.secondary)
+              .background(Color(.secondarySystemBackground))
+              .cornerRadius(10.0)
+
+              if showCancelButton  {
+                  Button("Cancel") {
+                          UIApplication.shared.endEditing(true) // this must be placed before the other commands here
+                          self.searchText = ""
+                          self.showCancelButton = false
+                  }
+                  .foregroundColor(Color(.systemBlue))
+              }
           }
-        )
+          .padding(.horizontal)
+          .navigationBarHidden(showCancelButton) // .animation(.default) // animation does not work properly
+
+            List(contactStore.contacts.filter{$0.name.hasPrefix(searchText) || searchText == "" }) { contact in
+            Text(contact.name)
+          }
+            
+            
+          .navigationBarTitle("Contact")
+          .navigationBarItems(
+            leading:
+              Button("Groups"){ },
+            trailing:
+              Button(
+              action: {}
+              ){
+              Image(systemName: "plus")
+            }
+          )
       }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ Contacts/Views/Extensions.swift
@@ -0,0 +1,33 @@
+//
+//  Extensions.swift
+//  Contacts
+//
+//  Created by Mo Abdulmalik on 27/02/2020.
+//  Copyright © 2020 Mo Abdulmalik. All rights reserved.
+//
+
+import SwiftUI
+
+extension UIApplication {
+    func endEditing(_ force: Bool) {
+        self.windows
+            .filter{$0.isKeyWindow}
+            .first?
+            .endEditing(force)
+    }
+}
+
+struct ResignKeyboardOnDragGesture: ViewModifier {
+    var gesture = DragGesture().onChanged{_ in
+        UIApplication.shared.endEditing(true)
+    }
+    func body(content: Content) -> some View {
+        content.gesture(gesture)
+    }
+}
+
+extension View {
+    func resignKeyboardOnDragGesture() -> some View {
+        return modifier(ResignKeyboardOnDragGesture())
+    }
+}
